ec1584394ef269e5edb9c21b9abd166c9c2ad929
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2025-04-10 16:39:41 +0100

    Remove logging in extract forms

--- tools/extractForms.ts
+++ tools/extractForms.ts
@@ -25,7 +25,6 @@ export async function extractForms(url: string, verbose: boolean): Promise<FormS
           type: element.getAttribute('type') || element.tagName.toLowerCase(),
         }))
     );
-    console.log(inputs);
     
     
     const forms = await page.$$eval('form', (formElements) =>
